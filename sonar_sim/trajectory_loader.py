3d67ba52f61c455e3df1ed96ece3bfeb7726d83b
David Botta <david.botta@gmx.ch>
2025-09-22 16:03:05 +0200

    added support for multy point trajectories

--- sonar_sim/trajectory_loader.py
+++ sonar_sim/trajectory_loader.py
@@ -2,7 +2,6 @@ import yaml
 import numpy as np
 from scipy.spatial.transform import Rotation as R, Slerp
 
-
 def load_trajectory(yaml_path: str):
     """
     Load trajectory from a YAML file and generate interpolated poses.
@@ -17,36 +16,58 @@ def load_trajectory(yaml_path: str):
     with open(yaml_path, 'r') as f:
         data = yaml.safe_load(f)
 
-    start_pos = np.array(data['start_pose']['position'])
-    start_rot = R.from_euler('xyz', data['start_pose']['rotation_euler'], degrees=True)
+    keyframe_positions = []
+    keyframe_rotations = []
 
-    end_pos = np.array(data['end_pose']['position'])
-    end_rot = R.from_euler('xyz', data['end_pose']['rotation_euler'], degrees=True)
+    for pose in data['poses']:
+        keyframe_positions.append(np.array(pose['position']))
+        rot = R.from_euler('xyz', pose['rotation_euler'], degrees=True)
+        keyframe_rotations.append(rot)
 
     total_time = data['total_time']
     timesteps = data['timesteps']
-
     dt = total_time / (timesteps - 1)
+
+    # Total number of keyframes
+    num_keyframes = len(keyframe_positions)
+    segment_count = num_keyframes - 1
+
+    # Time allocation: distribute timesteps evenly across segments
+    timesteps_per_segment = np.linspace(0, timesteps - 1, segment_count + 1, dtype=int)
 
     poses = []
 
-    # scipy Rotation.slerp requires a special call, so we build a slerp object
-    slerp = Slerp([0, 1], R.concatenate([start_rot, end_rot]))
+    for i in range(segment_count):
+        start_idx = timesteps_per_segment[i]
+        end_idx = timesteps_per_segment[i + 1]
+        steps = end_idx - start_idx + 1
 
-    for t in range(timesteps):
-        alpha = t / (timesteps - 1)
+        if steps <= 1:
+            continue  # Nothing to interpolate
 
-        # Interpolate position linearly
-        pos = (1 - alpha) * start_pos + alpha * end_pos
+        # Setup Slerp between current segment's rotations
+        slerp_times = [0, 1]
+        slerp_rots = R.concatenate([keyframe_rotations[i], keyframe_rotations[i + 1]])
+        slerp = Slerp(slerp_times, slerp_rots)
 
-        # Interpolate rotation via slerp
-        rot = slerp(alpha)
+        for j in range(steps):
+            alpha = j / (steps - 1)
 
-        # Compose homogeneous transformation matrix
-        pose_mat = np.eye(4)
-        pose_mat[:3, :3] = rot.as_matrix()
-        pose_mat[:3, 3] = pos
+            # Interpolate position
+            pos = (1 - alpha) * keyframe_positions[i] + alpha * keyframe_positions[i + 1]
 
-        poses.append(pose_mat)
+            # Interpolate rotation
+            rot = slerp(alpha)
+
+            # Compose transformation matrix
+            pose_mat = np.eye(4)
+            pose_mat[:3, :3] = rot.as_matrix()
+            pose_mat[:3, 3] = pos
+
+            # Avoid duplicate poses between segments
+            if i > 0 and j == 0:
+                continue
+
+            poses.append(pose_mat)
 
     return poses, dt
